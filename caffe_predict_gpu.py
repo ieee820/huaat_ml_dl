cc89cea4cf70e8b39ce27a971a89d91d8f6766bd
james_jjyang <ieee820@gmail.com>
2016-09-06 20:54:59 +0800

    Update caffe_predict_gpu.py

--- caffe_predict_gpu.py
+++ caffe_predict_gpu.py
@@ -3,7 +3,7 @@ import sys
 caffe_root = '/opt/caffe/'
 import caffe
 image_path = '/opt/cat/*'
-caffe.set_mode_cpu()
+#caffe.set_mode_cpu()
 print 'caffe load.. ',caffe
 #model def
 model_def = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
