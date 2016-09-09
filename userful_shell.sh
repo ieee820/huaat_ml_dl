da3c44de236ddffa3db24b83672d04920fecef4b
yangjj <ieee820@163.com>
2016-09-09 15:33:42 +0800

    dd

--- userful_shell.sh
+++ userful_shell.sh
@@ -3,3 +3,5 @@ cat caffe.log | awk -F"\t" '$1 ~ /cat/{print}' | awk -F"\t" '$2 !~ /cat/{print}'
 #测试caffe prediction
 python caffe_main_test02.py /root/test_dir /root/test.log
 
+#过滤文件
+find ./cat/ -iname "*.jpg" | sed -n '1,100p' | xargs cp -t ../temp/
